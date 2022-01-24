290af6d40ca5ef6632542f2bfa1fc86b12fc0a02
HiagoBriano <hiago.artist@hotmail.com>
2022-01-23 21:47:53 -0300

    add pages adicionado

---
+++ build/precache-manifest.888a766d6acd11730e480c8658508cc6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ef84d9062dadc031ef7152f817686b",
+    "url": "/projeto-trivia/index.html"
+  },
+  {
+    "revision": "ad52be92ddb760428931",
+    "url": "/projeto-trivia/static/css/main.38fe8184.chunk.css"
+  },
+  {
+    "revision": "7d6dcc67cd13dd9fc0c5",
+    "url": "/projeto-trivia/static/js/2.1e7a1143.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/projeto-trivia/static/js/2.1e7a1143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad52be92ddb760428931",
+    "url": "/projeto-trivia/static/js/main.2976927c.chunk.js"
+  },
+  {
+    "revision": "ebb0190f0742eb3f1899",
+    "url": "/projeto-trivia/static/js/runtime-main.f8589bed.js"
+  },
+  {
+    "revision": "9c8463bfb3e1c7295f3bff2abde8f34d",
+    "url": "/projeto-trivia/static/media/engrenagem3.9c8463bf.svg"
+  },
+  {
+    "revision": "c92bd01621db7a820ea0adce176c8d85",
+    "url": "/projeto-trivia/static/media/header-background.c92bd016.png"
+  },
+  {
+    "revision": "0293dbef05c17959ec0d13cafae1980c",
+    "url": "/projeto-trivia/static/media/token-viewer-background.0293dbef.png"
+  },
+  {
+    "revision": "8397b8e792042ccec9ed561bbd8920a0",
+    "url": "/projeto-trivia/static/media/trivia-01.8397b8e7.png"
+  }
+]);
